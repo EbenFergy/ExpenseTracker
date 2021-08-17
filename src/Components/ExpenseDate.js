54406dca09a8d01ab56619a6f5cc5e9191ea0592
Ferguson Osas <ebenezerosas92@gmail.com>
2021-08-17 16:22:50 +0100

    added styling and react

---
+++ src/Components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import './ExpenseDate.css'
+
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString("en-us", { month: "short" });
+  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className="calender">
+      <div className='month'>{month} </div>
+      <div className='day'>{day}</div>
+      <div className='year'>{year}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
---
+++ src/Components/ExpenseItem.jsx
@@ -0,0 +1,20 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+const ExpenseItem = (props) => {
+  // const {id, title, amount, date} = expenses_Items;
+  return (
+    <div className="expense-item">
+      <div className=''>
+        <ExpenseDate date={props.date} />
+      </div>
+
+      <div className="expense-item__description">
+        <h2 className="">{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
